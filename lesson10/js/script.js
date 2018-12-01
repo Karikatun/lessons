c0cf70c6180b796ae5f0e2589e70dcc98c0eadf1
Anton Shevchenko <41957518+Karikatun@users.noreply.github.com>
2018-12-01 16:06:54 +0300

    Дополнено обязательное задание урока 10

--- lesson10/js/script.js
+++ lesson10/js/script.js
@@ -112,35 +112,17 @@ window.addEventListener('DOMContentLoaded', function () {
         descrBtn[i].addEventListener('click', showTab);
     }
 
-    let anchors = document.querySelectorAll('a[href*="#"]'),
-        animationTime = 300,
-        framesCount = 20;
+    let anchorlinks = document.querySelectorAll('a[href^="#"]');
 
-
-    let linkNav = document.querySelectorAll('[href^="#"]'), //выбираем все ссылки к якорю на странице
-        V = 0.4; // скорость, может иметь дробное значение через точку (чем меньше значение - тем больше скорость)
-    for (let i = 0; i < linkNav.length; i++) {
-        linkNav[i].addEventListener('click', function (e) { //по клику на ссылку
-            e.preventDefault(); //отменяем стандартное поведение
-            let w = window.pageYOffset, // производим прокрутка прокрутка
-                hash = this.href.replace(/[^#]*(.*)/, '$1'); // к id элемента, к которому нужно перейти
-            t = document.querySelector(hash).getBoundingClientRect().top, // отступ от окна браузера до id
-                start = null;
-            requestAnimationFrame(step);
-
-            function step(time) {
-                if (start === null) {
-                    start = time;
-                }
-                let progress = time - start,
-                    r = (t < 0 ? Math.max(w - progress / V, w + t) : Math.min(w + progress / V, w + t));
-                window.scrollTo(0, r);
-                if (r != w + t) {
-                    requestAnimationFrame(step);
-                } else {
-                    location.hash = hash; // URL с хэшем
-                }
-            }
-        }, false);
+    for (let item of anchorlinks) { 
+        item.addEventListener('click', (e) => {
+            e.preventDefault();
+            let hashval = item.getAttribute('href');
+            let target = document.querySelector(hashval);
+            target.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        });
     }
 });
